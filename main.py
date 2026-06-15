5001ce5cd7104e90e7a36459f896e06b7f7254cc
Givon Zirkind <givonzirkdin@gmail.com>
2026-06-15 18:29:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="ChatPDF")
+
+
+def display_messages():
+    st.subheader("Chat")
+    for i, (msg, is_user) in enumerate(st.session_state["messages"]):
+        message(msg, is_user=is_user, key=str(i))
+    st.session_state["thinking_spinner"] = st.empty()
+
+def page():
+    if len(st.session_state) == 0:
+        st.session_state["messages"] = []
+        st.session_state["assistant"] = ChatPDF()
+
+    st.header("ChatPDF")
