c7688625892fe51ffb8185dc436a74e40bc63916
Givon Zirkind <givonzirkdin@gmail.com>
2026-06-15 18:32:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def display_messages():
         message(msg, is_user=is_user, key=str(i))
     st.session_state["thinking_spinner"] = st.empty()
 
+def process_input():
+    if st.session_state["user_input"] and\
+       len(st.session_state["user_input"].strip()) > 0:
+        user_text = st.session_state["user_input"].strip()
+        with st.session_state["thinking_spinner"], st.spinner("Thinking..."):
+            agent_text = st.session_state["assistant"].ask(user_text)
+
+        st.session_state["messages"].append((user_text, True))
+        st.session_state["messages"].append((agent_text, False))
+
 def page():
     if len(st.session_state) == 0:
         st.session_state["messages"] = []
